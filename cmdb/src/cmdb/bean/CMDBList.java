c3c90d346063d7c49fa0ef47865f4fce7776bdaf
krishnendubose <32927331+krishnendubose@users.noreply.github.com>
2018-05-10 18:12:20 +0530

    Update CMDBList.java

--- cmdb/src/cmdb/bean/CMDBList.java
+++ cmdb/src/cmdb/bean/CMDBList.java
@@ -1,11 +1,16 @@
 package cmdb.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CMDBList implements Serializable {
 
 	public List<CMDB> cmdbList;
+
+	public CMDBList(){
+		cmdbList = new ArrayList<CMDB>();
+	}
 
 	public List<CMDB> getCmdbList() {
 		return cmdbList;
@@ -14,5 +19,13 @@ public class CMDBList implements Serializable {
 	public void setCmdbList(List<CMDB> cmdbList) {
 		this.cmdbList = cmdbList;
 	}
-	
+	public String toString(){
+
+		StringBuffer cmdbListData = new StringBuffer();
+		for (CMDB data : cmdbList){
+			cmdbListData = cmdbListData.append(data + "\n");
+		}
+		return cmdbListData.toString();
+
+	}
 }
