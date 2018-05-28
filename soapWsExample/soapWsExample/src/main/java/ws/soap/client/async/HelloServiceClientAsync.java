48a60ab0ee76708a92335d1b3b104c7c3612b40e
krishnendubose <32927331+krishnendubose@users.noreply.github.com>
2018-05-28 16:07:02 +0530

    Update HelloServiceClientAsync.java

--- soapWsExample/soapWsExample/src/main/java/ws/soap/client/async/HelloServiceClientAsync.java
+++ soapWsExample/soapWsExample/src/main/java/ws/soap/client/async/HelloServiceClientAsync.java
@@ -3,35 +3,39 @@ package ws.soap.client.async;
 import javax.xml.ws.AsyncHandler;
 import javax.xml.ws.Response;
 
-import ws.soap.client.async.HelloService;
-import ws.soap.client.async.HelloServiceService;
-
 public class HelloServiceClientAsync {
 
 	public static void main(String[] args) {
-
 		HelloServiceService service = new HelloServiceService();
 		HelloService port = service.getHelloServicePort();
 		port.sayHelloWorldAsync(new MyHandler());
 		try{
-			System.out.println("Called sayHelloWorldAsync(), running the next statement");
-		}catch(Exception e ){
+			System.out.println("Main Thread name:- "   + Thread.currentThread().getName());
+			Thread.sleep(500);
+		}catch(Exception e){
 			e.printStackTrace();
 		}
-	}
 
+	}
+	/**
+	 * Though this handler is async , unless the main thread has any specific task to do it will terminate evetually causing the other threads to also exit.
+	 * To test the async functionality therefore we have to keep the main thread busy. 	  
+	 *
+	 */
 	static class MyHandler implements AsyncHandler<SayHelloWorldResponse>{
 
+		@Override
 		public void handleResponse(Response<SayHelloWorldResponse> arg0) {
-
+			System.out.println("Within MyHandler class's handleResponse() method, Thread name:- " + Thread.currentThread().getName());
 			try{
-				SayHelloWorldResponse response = arg0.get();
-				System.out.println("Response from hello world service after the async call :- " + response.getReturn());
-			}catch (Exception e) {
+				SayHelloWorldResponse sayHelloWorldResponse = arg0.get();
+				System.out.println("Details:- \n" + sayHelloWorldResponse.getReturn());
+			}catch(Exception e){
 				e.printStackTrace();
 			}
 
 		}
+
 	}
 
 }
